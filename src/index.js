f7596c2188aad785a61fd699644e442347aeeddd
ben9198 <bschroeder@esri.com>
2017-11-04 16:39:33 -0400

    Fix images

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
 import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
+/* This code is needed to properly load the images in the Leaflet CSS */
+delete L.Icon.Default.prototype._getIconUrl;
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
+  iconUrl: require('leaflet/dist/images/marker-icon.png'),
+  shadowUrl: require('leaflet/dist/images/marker-shadow.png'),
+});
 
 const map = L.map('map');
 const defaultCenter = [38.889269, -77.050176];
